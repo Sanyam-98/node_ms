38b7053edd4d904fcd53bf2de8b6f312b1fed1e3
sanyam shah <sanyam.shah@techifysolutions.com>
2024-04-09 21:43:06 +0530

    added hello-2-3

--- ms1/index.js
+++ ms1/index.js
@@ -44,6 +44,26 @@ app.get("/api/v1/ms1/hello-3", async (req, res) => {
 });
 
 
+app.get("/api/v1/ms1/hello-2-3", async (req, res) => {
+  try {
+      let obj = await axios({
+        method: 'get',
+        url: `${MS_GATEWAY}/api/v1/ms2`,
+      });
+      console.log(obj?.data, 53);
+      let obj2 = await axios({
+        method: 'get',
+        url: `${MS_GATEWAY}/api/v1/ms3`,
+      });
+      console.log(obj2?.data, 58);
+      return res.status(200).json({...obj?.data, m2: obj2?.data?.m, n:'ms1/hello-2-3'});
+  } catch (e) {
+    console.log(`Error fetching res`, e);
+    return res.status(500).json({message: "error fetching hello-2-3", data: e.response});
+  }
+});
+
+
 app.listen(APP_PORT, () => {
   console.log(`App listening at http://localhost:${APP_PORT}`);
 });
